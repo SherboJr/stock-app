d0b63fbfe39e551082dd3ee5e1b9c6f3c4409e90
SherboJr <mosherb2004@gmail.com>
2024-02-20 14:49:52 +0200

    fixes

--- src/app/(Screens)/Home.tsx
+++ src/app/(Screens)/Home.tsx
@@ -1,72 +1,281 @@
-// // pages/StocksPage.tsx
+// "use client";
+// import { useState, useEffect, useRef } from "react";
+// import Sheet from "react-modal-sheet";
+// import SplashScreen from "./(Screens)/Splash";
+// import React from "react";
+// import "firebase/firestore";
 
-// import { useState } from "react";
-// import StockDetails from "./stockDetails";
-// const StocksPage: React.FC = () => {
+// const Home: React.FC = () => {
 //   const [selectedStock, setSelectedStock] = useState<{
-//     logoUrl?: string;
 //     ticker: string;
-//     closePrice: number;
-//     priceChangePercentage: number;
+//     name: string;
+//     market: string;
+//     locale: string;
+//     primary_exchange: string;
+//     type: string;
+//     active: boolean;
+//     currency_name: string;
+//     cik: string;
+//     composite_figi: string;
+//     share_class_figi: string;
+//     market_cap: number;
+//     phone_number: string;
 //     description: string;
-//     statistics: {
-//       close: number;
-//       open: number;
-//       high: number;
-//       low: number;
+//     sic_code: string;
+//     sic_description: string;
+//     ticker_root: string;
+//     homepage_url: string;
+//     total_employees: number;
+//     list_date: string; // You might want to use a Date type for this
+//     branding: {
+//       logo_url: string;
+//       icon_url: string;
 //     };
-//     website?: string;
+//     share_class_shares_outstanding: number;
+//     weighted_shares_outstanding: number;
+//     round_lot: number;
 //   } | null>(null);
 
-//   const handleStockClick = (stock: (typeof stocksData)[0]) => {
-//     // Create a new object with the necessary properties
-//     const selectedStockData = {
-//       logoUrl: undefined, // Replace with the actual logo URL
-//       ticker: stock.ticker,
-//       closePrice: 0, // Replace with the actual close price
-//       priceChangePercentage: 0, // Replace with the actual price change percentage
-//       description: "", // Replace with the actual description
-//       statistics: {
-//         close: 0, // Replace with the actual close statistic
-//         open: 0, // Replace with the actual open statistic
-//         high: 0, // Replace with the actual high statistic
-//         low: 0 // Replace with the actual low statistic
-//       },
-//       website: undefined // Replace with the actual website URL
+//   const [stockData, setStockData] = useState<any[]>([]);
+//   const [searchTerm, setSearchTerm] = useState<string>("");
+//   const [loading, setLoading] = useState(true);
+
+//   const [isOpen, setOpen] = useState(false);
+//   const observer = useRef<IntersectionObserver | null>(null);
+
+//   useEffect(() => {
+//     const fetchStockData = async () => {
+//       try {
+//         const response = await fetch(
+//           `https://api.polygon.io/v3/reference/tickers?active=true&limit=20&apiKey=NZqSkFngRrHtDAM4CO22GEvpby_YyhCp`
+//         );
+//         const data = await response.json();
+//         console.log("Fetched Data:", data);
+//         setStockData(data.results || []);
+//       } catch (error) {
+//         console.error("Error fetching stock data:", error);
+//       }
 //     };
 
-//     // Set the selected stock to open the StockDetails bottom sheet
-//     setSelectedStock(selectedStockData);
+//     fetchStockData();
+//   }, []);
+
+//   useEffect(() => {
+//     const timer = setTimeout(() => {
+//       setLoading(false);
+//     }, 3000);
+
+//     return () => clearTimeout(timer);
+//   }, []);
+
+//   const handleStockClick = async (stock: any) => {
+//     try {
+//       const response = await fetch(
+//         `https://api.polygon.io/v3/reference/tickers/${stock.ticker}?apiKey=NZqSkFngRrHtDAM4CO22GEvpby_YyhCp`
+//       );
+//       const details = await response.json();
+//       console.log(details),
+//         setSelectedStock({
+//           ticker: stock.ticker,
+//           name: stock.name,
+//           market: stock.market,
+//           locale: stock.locale,
+//           primary_exchange: stock.primary_exchange,
+//           type: stock.type,
+//           active: stock.active,
+//           currency_name: stock.currency_name,
+//           cik: stock.cik,
+//           composite_figi: stock.composite_figi,
+//           share_class_figi: stock.share_class_figi,
+//           market_cap: stock.market_cap,
+//           phone_number: stock.phone_number,
+//           description: stock.description,
+//           sic_code: stock.sic_code,
+//           sic_description: stock.sic_description,
+//           ticker_root: stock.ticker_root,
+//           homepage_url: stock.homepage_url,
+//           total_employees: stock.total_employees,
+//           list_date: stock.list_date,
+//           branding: {
+//             logo_url: stock.logo_url,
+//             icon_url: stock.icon_url
+//           },
+//           share_class_shares_outstanding: stock.share_class_shares_outstanding,
+//           weighted_shares_outstanding: stock.weighted_shares_outstanding,
+//           round_lot: stock.round_lot
+//         });
+
+//       setOpen(true);
+//     } catch (error) {
+//       console.error("Error fetching stock details:", error);
+//     }
 //   };
 
-//   const stocksData = [
-//     { name: "Company A", ticker: "AAPL", id: "1" },
-//     { name: "Company B", ticker: "GOOGL", id: "2" },
-//     { name: "Company C", ticker: "MSFT", id: "3" }
-//     // Add more data as needed
-//   ];
+//   const fetchMoreStocks = async () => {
+//     try {
+//       const response = await fetch(
+//         `https://api.polygon.io/v3/reference/tickers?active=true&limit=20&apiKey=NZqSkFngRrHtDAM4CO22GEvpby_YyhCp`
+//       );
+//       const data = await response.json();
+//       console.log("Fetched More Data:", data);
+//       setStockData((prevData) => [...prevData, ...(data.results || [])]);
+//     } catch (error) {
+//       console.error("Error fetching more stock data:", error);
+//     }
+//   };
 
+//   const lastStockRef = useRef<HTMLDivElement | null>(null);
+//   const searchStocks = async (term: string) => {
+//     try {
+//       const response = await fetch(
+//         `https://api.polygon.io/v3/reference/tickers?search=${term}&active=true&limit=1&apiKey=NZqSkFngRrHtDAM4CO22GEvpby_YyhCp`
+//       );
+//       const data = await response.json();
+//       console.log("Searched Data:", data);
+//       setStockData(data.results || []);
+//     } catch (error) {
+//       console.error("Error searching stocks:", error);
+//     }
+//   };
+//   useEffect(() => {
+//     if (lastStockRef.current) {
+//       observer.current = new IntersectionObserver(
+//         (entries) => {
+//           if (entries[0].isIntersecting) {
+//             fetchMoreStocks();
+//           }
+//         },
+//         {
+//           threshold: 0.1
+//         }
+//       );
+
+//       observer.current.observe(lastStockRef.current);
+//     }
+
+//     return () => {
+//       if (observer.current) {
+//         observer.current.disconnect();
+//       }
+//     };
+//   }, [lastStockRef.current]);
+//   const handleSearchTermChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+//     const term = e.target.value;
+//     setSearchTerm(term);
+//     searchStocks(term);
+//   };
 //   return (
-//     <div className="container mx-auto mt-8 p-4">
-//       {/* ... Existing code ... */}
-
-//       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-//         {stocksData.map((stock, index) => (
-//           <div
-//             key={index}
-//             className="bg-gray-200 p-4 rounded-md shadow-md cursor-pointer"
-//             onClick={() => handleStockClick(stock)}
-//           >
-//             <h2 className="text-lg font-semibold mb-2">{stock.name}</h2>
-//             <p className="text-sm text-gray-600">{stock.ticker}</p>
+//     <div>
+//       {loading ? (
+//         <SplashScreen />
+//       ) : (
+//         <div className="">
+//           <div className=" flex flex-row bg-slate-900 w-full h-20">
+//             <img src="/nlogo.png" alt="pepe" className=" h-8 w-8 ml-4 mt-3 " />
+//             <div className="text-2xl font-mono text-white pt-3 ">Nasdaq</div>
 //           </div>
-//         ))}
-//       </div>
 
-//       {/* Open StockDetails bottom sheet when a stock is selected */}
-//       {selectedStock && <StockDetails stockData={selectedStock} />}
+//           <div className=" mt-5 mx-3">
+//             <input
+//               type="text"
+//               placeholder="Search for stocks"
+//               className="border border-gray-300 p-3 mb-5 rounded-full w-full bg-slate-700"
+//               value={searchTerm}
+//               onChange={handleSearchTermChange}
+//             />
+
+//             <div className="grid grid-cols-2 gap-4">
+//               {stockData.map((stock: any, index: number) => (
+//                 <div
+//                   key={index}
+//                   className=" bg-slate-700 p-4 rounded-md shadow-md cursor-pointer my-3"
+//                   onClick={() => handleStockClick(stock)}
+//                 >
+//                   <div className=" ml-11 mb-3 h-12 w-12 rounded-lg border-solid border-2 border-gray-500  text-center ">
+//                     <div className=" mt-4 overflow-hidden text-xs text-gray-300">
+//                       {stock.ticker}
+//                     </div>
+//                   </div>
+//                   <h2 className="text-lg mb-2 text-white text-center">
+//                     {stock.ticker}
+//                   </h2>
+//                   <p className="text-sm mb-2 text-gray-500 text-center">
+//                     {stock.name}
+//                   </p>
+
+//                   {/* Display more details or customize as needed */}
+
+//                   {/* Add more details as needed */}
+//                 </div>
+//               ))}
+//             </div>
+//             <div ref={lastStockRef}></div>
+//             <Sheet isOpen={isOpen} onClose={() => setOpen(false)}>
+//               <Sheet.Container>
+//                 <Sheet.Content className="bg-slate-800">
+//                   <div className="mx-5 my-3 text-white text-2xl">
+//                     {selectedStock?.homepage_url}
+//                     {selectedStock?.name}
+//                     {selectedStock?.ticker}
+//                     {selectedStock?.active}
+//                     {selectedStock?.currency_name}
+//                     {selectedStock?.market_cap}
+//                     {selectedStock?.weighted_shares_outstanding}
+//                     {selectedStock?.sic_code}
+//                   </div>
+//                   <div className="mx-5 my-3 text-white text-2xl">
+//                     {selectedStock?.primary_exchange}
+//                     {selectedStock?.weighted_shares_outstanding}
+//                   </div>
+
+//                   <div className="mx-5 text-gray-500 text-base">
+//                     description
+//                   </div>
+//                   <div className="mx-5 my-3 text-white text-2xl">
+//                     Statistics
+//                   </div>
+//                   <div className=" grid-flow-col grid-cols-2 justify-between">
+//                     <div className="mx-5 text-gray-500 text-base flex flex-col">
+//                       Open
+//                     </div>
+//                     <div className="mx-5 text-white text-base">
+//                       {selectedStock?.type}
+//                     </div>
+//                     <div className="mx-5 text-gray-500 text-base flex flex-col">
+//                       Shares
+//                     </div>
+//                     <div className="mx-5 text-white text-base">
+//                       {" "}
+//                       {selectedStock?.market}
+//                     </div>
+//                     <div className="mx-5 text-gray-500 text-base flex flex-col">
+//                       Close Price:
+//                     </div>
+//                     <div className="mx-5 text-white text-base">
+//                       {selectedStock?.homepage_url}
+//                     </div>
+//                     <div className="mx-5 text-gray-500 text-base flex flex-col">
+//                       Highest Price:
+//                     </div>
+//                     <div className="mx-5 text-white text-base">
+//                       {selectedStock?.phone_number}
+//                     </div>
+//                     <div className="mx-5 text-gray-500 text-base flex flex-col">
+//                       Lowest Price:
+//                     </div>
+//                     <div className="mx-5 text-white text-base">
+//                       {selectedStock?.list_date}
+//                     </div>
+//                   </div>
+//                 </Sheet.Content>
+//               </Sheet.Container>
+//               <Sheet.Backdrop />
+//             </Sheet>
+//           </div>
+//         </div>
+//       )}
 //     </div>
 //   );
 // };
 
-// export default StocksPage;
+// export default Home;
--- src/app/pages/api/sendData/route.ts
+++ src/app/pages/api/sendData/route.ts
@@ -1,27 +1,27 @@
-import { doc } from "firebase/firestore";
-const {
-  initializeApp,
-  applicationDefault,
-  cert
-} = require("firebase-admin/app");
-const {
-  getFirestore,
-  Timestamp,
-  FieldValue,
-  Filter
-} = require("firebase-admin/firestore");
-const serviceAccount = require("/Next Nasdaq/stock-app/DB-KEY.json");
+// import { doc } from "firebase/firestore";
+// const {
+//   initializeApp,
+//   applicationDefault,
+//   cert
+// } = require("firebase-admin/app");
+// const {
+//   getFirestore,
+//   Timestamp,
+//   FieldValue,
+//   Filter
+// } = require("firebase-admin/firestore");
+// const serviceAccount = require("/Next Nasdaq/stock-app/DB-KEY.json");
 
-initializeApp({
-  credential: cert(serviceAccount)
-});
+// initializeApp({
+//   credential: cert(serviceAccount)
+// });
 
-const db = getFirestore();
-const docRef = db.collection("userData").doc("qwPDPQhVQFrDHtiHDLjh");
+// const db = getFirestore();
+// const docRef = db.collection("userData").doc("qwPDPQhVQFrDHtiHDLjh");
 
-export async function POST(req, res) {
-  const data = await req.json();
-  console.log(data);
+// export async function POST(req, res) {
+//   const data = await req.json();
+//   console.log(data);
 
-  return Response.json(data);
-}
+//   return Response.json(data);
+// }
